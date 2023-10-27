55387140ea988e8689006c8d399083093860dcb4
kunal pandey <kp7834810@gmail.com>
2023-10-27 17:30:50 +0530

    Frontend committed successfully

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import ToDo from "./components/ToDo";
+import {
+  addTodo,
+  getAlltodo,
+  updateTodo,
+  deleteTodo,
+} from "./components/Utils/HandleAPI";
 
 function App() {
+  const [toDo, setToDo] = useState([]);
+  const [text, setText] = useState("");
+  const [isUpdating, setisUpdating] = useState(false);
+  const [todoID, settodoID] = useState();
+
+  // mounts the fetched data on to the screen
+  useEffect(() => {
+    getAlltodo(setToDo);
+  }, []);
+
+  const updateMode = (_id, text) => {
+    setisUpdating(true);
+    setText(text);
+    settodoID(_id);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>ToDo App</h1>
+        <div className="top">
+          <input
+            type="text"
+            placeholder="Add your Daily Task here"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <div
+            className="add"
+            onClick={
+              isUpdating
+                ? () =>
+                    updateTodo(todoID, setisUpdating, text, setText, setToDo)
+                : () => addTodo(text, setText, setToDo)
+            }
+          >
+            {isUpdating ? "Update Task" : "Add Task"}
+          </div>
+        </div>
+        {/* mapping out the data for rendering  */}
+        <div className="List">
+          {toDo.map((item) => (
+            <ToDo
+              key={item._id}
+              text={item.text}
+              updateMode={() => updateMode(item._id, item.text)}
+              deleteTodo={() => deleteTodo(item._id, setToDo)}
+            />
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Utils/HandleAPI.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+
+const baseUrl = "http://localhost:5000";
+
+// fetching the TODO from Database
+
+const getAlltodo = async (setToDo) => {
+  try {
+    const { data } = await axios.get(baseUrl);
+    console.log(data);
+    setToDo(data); // Assuming setTodo is defined elsewhere
+  } catch (error) {
+    console.error("Error fetching todos: ", error);
+  }
+};
+
+// Adding the TODO to the Database and after adding just calling the fetch function
+
+const addTodo = async (text, setText, setToDo) => {
+  try {
+    const response = await axios.post(`${baseUrl}/save`, { text });
+    console.log(response.data);
+    setText("");
+    getAlltodo(setToDo);
+  } catch (error) {
+    console.error("Error adding todos: ", error);
+  }
+};
+
+const updateTodo = async (todoId, setisUpdating, text, setText, setToDo) => {
+  try {
+    const response = await axios.post(`${baseUrl}/update`, {
+      _id: todoId,
+      text,
+    });
+    console.log(response.data);
+    setText("");
+    setisUpdating(false);
+    getAlltodo(setToDo);
+  } catch (error) {
+    console.error("Error adding todos: ", error);
+  }
+};
+const deleteTodo = async (todoId, setToDo) => {
+  try {
+    const response = await axios.post(`${baseUrl}/delete`, {
+      _id: todoId,
+    });
+    console.log(response.data);
+    getAlltodo(setToDo);
+  } catch (error) {
+    console.error("Error adding todos: ", error);
+  }
+};
+
+export { getAlltodo, addTodo, updateTodo, deleteTodo };
