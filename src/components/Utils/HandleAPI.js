6412f136473084a5e95f9552d83efd015ea6ccc5
kunal pandey <kp7834810@gmail.com>
2023-11-01 10:42:54 +0530

    fixed the routing issues

--- src/components/Utils/HandleAPI.js
+++ src/components/Utils/HandleAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5000";
+const baseUrl = "https://fullstack-todo-list-app-backend.onrender.com";
 
 // fetching the TODO from Database
 
